b84bc3555a33864412bfecb2875aee051c0f24b0
Andrey Kovalev <andrey.kovalev@imc.com>
2023-05-07 17:27:25 +0200

    api client example

--- petmanagerclient.py
+++ petmanagerclient.py
@@ -1,79 +1,32 @@
-from modules.datamodel import Animal, Result
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+logger=logging.getLogger(__name__)
+
+from modules.datamodel import Animal
 from typing import List, Tuple
 
+SERVER='http://localhost:....'
 
-class PetManager:
-    def __init__(self) -> None:
-        self.zoo = []
+class PetManagerClient:
+    def __init__(self, server) -> None:
+        logger.debug(f"petmanager init, server={server}")
+        self.server=server
 
-    def _validate(self, animal: Animal) -> bool:
-        """check if we can accept this animal.
-        Checks:
-            1. species is "dog" or "cat"
-            2. id is unique
-        """
-        existing_ids = set(map(lambda x: x.id, self.zoo))
-        if animal.id in existing_ids:
-            return False
-        return True
 
     def add_animal(self, animal: Animal) -> bool:
-        if  self._validate(animal):
-            self.zoo.append(animal)
-            return True
-        return False
+        r=requests.post(server+'/add', json=animal)
+        return r.json()
+        
+        
 
     def list_animals(self, species: str = None, gender: bool = None) -> List[Animal]:
-        """
-        return list of animal objects, if "species"or "gender" are specified - return only
-        animals with specified parameters. If none found = return an empty array.
-        """
-        animals=list(filter(lambda x: x.species==species or species == None, self.zoo))
         
-        result=list(filter(lambda x: x.gender_male==gender or gender == None, animals))
-
-        return result
-        # result = []
-        # print (self.zoo)
-        # Your code here
-        # return result
 
     def get_animal(self, id: int = None, name: str = None) -> Animal:
-        if id != None:
-            result=list(filter(lambda x: x.id == id, self.zoo))
-            
-    
-        if name != None:
-            result=list(filter(lambda x: x.name == name, self.zoo))
-
-            return result
-
-        """for i in self.zoo:   
-            if i == id:
-                print("YES")
-            else:
-                print("NO")"""
-        
-        """
-        Return requested animal and search result.
-
-        If id is passed - return the animal with given id
-        if id and name are passed - return the animal with given id and given name, if exists
-        if only name is given - return the only animal with this name. If more then 1 animals
-        have this name - fail.
-        The method should return object of type Animal if search was successful or None.
-        """
-        # return result
 
     def delete_animal(self, deleted: int) -> bool:
-        orig_len=len(self.zoo)
-        self.zoo=list(filter(lambda x: x.id != deleted, self.zoo))
-        return  len(self.zoo) != orig_len
-        """
-        Delete from the list the animal with given id, if found.
-        Return Result object with success=True if the animal deleted, else with success=False
-        """
-
 
 class Commands:
     add = 1
@@ -85,7 +38,7 @@ class Commands:
 
 if __name__ == "__main__":
     print("Starting Pet Manager")
-    mgr = PetManager()
+    mgr = PetManagerClient()
 
     menu = """
     1) add animal
