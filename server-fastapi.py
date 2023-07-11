5014461cdd7aba4ed09ff1ae50dc22b770cf9ea1
a <andrew.v.kovalev@gmail.com>
2023-07-11 21:58:33 +0200

    typing of return values

--- server-fastapi.py
+++ server-fastapi.py
@@ -1,28 +1,20 @@
 from fastapi import FastAPI
 from petmanager import PetManager
 from modules.datamodel import Animal
+from typing import List
 
 
 app = FastAPI()
 app.mgr=PetManager()
 
 @app.get("/api/v1/list")
-async def list():
+async def list() -> List(Animal):
     return app.mgr.list_animals()
 
 @app.put('/api/v1/add')
-async def create(animal: Animal):
+async def create(animal: Animal) -> bool:
     return app.mgr.add_animal(animal)
 
 @app.delete('/api/v1/delete/{id}')
-async def delete(id: int):
+async def delete(id: int) -> bool:
     return app.mgr.delete_animal(id)
-
-    #TESTTESTTEST
-
-#To run:
-#pip install uvicorn
-#uvicorn server-fastapi:app
-
-# http://localhost:8000/docs 
-# https://fastapi.tiangolo.com/
