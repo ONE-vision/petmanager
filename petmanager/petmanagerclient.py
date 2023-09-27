adeb9ea9003cb64c40e23d1a527f4150433739bd
ONE-Vision <mrpaulsmith@yandex.ru>
2023-09-27 20:14:53 +0300

    Trying to fix client

--- petmanager/petmanagerclient.py
+++ petmanager/petmanagerclient.py
@@ -15,20 +15,19 @@ class PetManagerClient:
         logger.debug(f"petmanager init, server={server}")
         self.server=server
 
-
     def add_animal(self, animal: Animal) -> bool:
         r=requests.post(server+'/add', json=animal)
         return r.json()
-        
+
     def list_animals(self, species: str = None, gender: bool = None) -> List[Animal]:
         r=requests.post(server+'/list', json=animal)
         return r.json()
 
-    def del_animal(self, id: int = None, name: str = None) -> Animal:
+    def del_animal(self, id: int = None) -> bool:
         r=requests.post(server+'/del', json=animal)
         return r.json()
-    
-    
+
+
 class Commands:
     add = 1
     search = 2
@@ -40,14 +39,6 @@ class Commands:
 if __name__ == "__main__":
     print("Starting Pet Manager")
     mgr = PetManagerClient()
-
-    menu = """
-    1) add animal
-    2) search for animal
-    3) lsit all animals
-    4) delete an animal
-    5) exit
-    """
 
     w = """Welcome to Pet manager! Please, choose command:
     1 - add animal;
@@ -79,32 +70,32 @@ if __name__ == "__main__":
             else:
                 print(f"Error: {result.message}")
 
-    if command == Commands.list:
-        # al=mgr.list_animals(species != None) - Trying to insert values to method_list, but it doesn't work now.
-        g=input("Gender (M/F, empty for all)")# .strip()
-        # g M, F, ""
-        #True = M, False = F, None = ""
-        #gm= ....... 
-        print(f"DEBUG: gm={gm}")
-        sp=input("Species (empty for all): ")
-        sp=sp if sp != "" else None
-        print(f"DEBUG: sp={sp}")
+        if command == Commands.list:
+            # al=mgr.list_animals(species != None) - Trying to insert values to method_list, but it doesn't work now.
+            g=input("Gender (M/F, empty for all)")# .strip()
+            # g M, F, ""
+            #True = M, False = F, None = ""
+            #gm= ....... 
+            print(f"DEBUG: gm={gm}")
+            sp=input("Species (empty for all): ")
+            sp=sp if sp != "" else None
+            print(f"DEBUG: sp={sp}")
         
         
         al=mgr.list_animals(gender=gm, species=sp)
         print(al)
-    if command == Commands.exit:
-        print("Goodbye!")
-        quit()
+        if command == Commands.exit:
+            print("Goodbye!")
+            quit()
     # Other commands here
-    if command == Commands.delete:
-        print ("Input ID:")
-        id_to_delete=int(input())
-        de=mgr.delete_animal(id_to_delete)
-        print(de)
+        if command == Commands.delete:
+            print ("Input ID:")
+            id_to_delete=int(input())
+            de=mgr.delete_animal(id_to_delete)
+            print(de)
 
-    if command == Commands.search:
-        id_to_search=int(input("Inpui ID: "))
-        name_to_search=str(input("Input Name: "))
-        se=mgr.get_animal(id_to_search, name_to_search)
-        print(se)
+        if command == Commands.search:
+            id_to_search=int(input("Inpui ID: "))
+            name_to_search=str(input("Input Name: "))
+            se=mgr.get_animal(id_to_search, name_to_search)
+            print(se)
